1186f0d1cfc5fe9e46781c6b2d54aff3d6ebc582
Alexitohp17 <alexhp.17.98@gmail.com>
2019-03-15 14:16:50 -0600

    Semi cambio antes de la birria

--- leerArchivoTexto/src/ejercicio1/Archivo.java
+++ leerArchivoTexto/src/ejercicio1/Archivo.java
@@ -6,14 +6,11 @@
 package ejercicio1;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
-import jdk.nashorn.internal.runtime.regexp.RegExp;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**Equipo: María Fernanda Román Hernández.
  *         Luis Alejandro Hinojosa Pérez.
@@ -29,6 +26,10 @@ public class Archivo {
     public String l;
     public int c=0; 
     public String cad = " "+l;
+    public static ArrayList <String> reservao= new ArrayList<>
+            //Se cambiaron estos simbolos porque java no los reconoce "°"->"@" "°°"->"@@" y "¬"->"%" "¬¬"->"%%"
+    (Arrays.asList("@","@@", "¬","¬¬","CON","EN","DEC","CAR","CAD","~","^","/","¿",
+                   "#","->>","FI","=|","&","//","Y","=>","Block","NF","X","M","S"));
     
     public Archivo()
     { 
@@ -59,7 +60,10 @@ public class Archivo {
     public String generarCadena(){
         String supercadena = "";
         char cad=leer(cont++);
-       
+        if(cad ==' ')
+        
+        cad=leer(cont++);
+            
          if((cad>=65 && cad<=90) || (cad>=97 && cad<=122))
          {
             //Esto es para las letra .
@@ -72,6 +76,7 @@ public class Archivo {
             leer(cont--);
             return supercadena;
         }
+         
         else if(cad>=48 && cad<=57 )
         {
             //Esto es para un número
@@ -96,19 +101,149 @@ public class Archivo {
                     cad=leer(cont++);
                 }  
               }
+              
+               //para concatenar los caracteres especiales 
               else
                 { 
                     leer(cont--);
                 }
                 leer(cont--);
             }
-           return supercadena;
+            
+            //Verificamos si un número tiene letras, lo cual no es valido
+//            if((cad>=65 && cad<=90) || (cad>=97 && cad<=122))
+//            {
+//                while((cad>=65 && cad<=90) || (cad>=97 && cad<=122))
+//                {
+//                  supercadena+=cad;
+//                  cad=leer(cont++);  
+//
+//                }
+//            
+//                leer(cont--);
+//                System.out.println("No es valido"); 
+//                return supercadena;
+//            
+//            }
+//             
+//           return supercadena;
         } 
         else 
         {
+            boolean bandera=true;
             supercadena+=cad;
-            return supercadena;
-        
+            
+            if(bandera==true)
+            {
+                switch(cad)
+                {
+                // (//)
+                case 47:              
+                  cad=leer(cont++);
+                    if(cad==47)
+                    {
+                        supercadena+=cad;
+                        cad=leer(cont++);
+                        
+                    }   
+                        leer(cont--);
+                        return supercadena;
+                
+                // %%
+                case 37:
+                    
+                    cad=leer(cont++);
+                        
+                        if(cad==37)
+                        {
+                            supercadena+=cad;
+                         
+                            cad=leer(cont++);
+                        }
+                    cad=leer(cont--);
+                    return supercadena;        
+                   
+                    // =| o =>
+                    case 61:
+                        cad=leer(cont++);
+                        if(cad==124)
+                            {
+                                supercadena+=cad;
+                                cad=leer(cont++);   
+                            }
+
+                        else if(cad== 62)
+                            {
+                                supercadena+=cad;
+                                cad=leer(cont++);
+                            }
+                            cad=leer(cont--);
+                            return supercadena;
+                        
+                        
+                        //->>
+                        case 45:
+                        cad=leer(cont++);
+                        
+                        if(cad==62)
+                        {
+                            supercadena+=cad;
+                            cad=leer(cont++);
+                            if(cad==62)
+                            {
+                                supercadena+=cad;
+                                cad=leer(cont++);
+                            }
+                        }
+                            cad=leer(cont--);
+                            return supercadena;
+                        
+                        //@@
+                        case 64:
+                        cad=leer(cont++);
+                        if(cad==64)
+                        {
+                            supercadena+=cad;
+                         
+                            cad=leer(cont++);
+                        }
+                        cad=leer(cont--);
+                        return supercadena;        
+                   
+                        
+                    }      
+            }
         }
-    }          
+         return supercadena;
+    }
+    
+           
+    public boolean verificar()
+    {
+        boolean bandera=false;                
+//            if(reservao.contains(caracter))
+//            {      
+//               System.out.println("Palabra o simbolo reservado"); 
+//            }
+//            else
+//            {   
+//                System.out.println("Palabra no encontrada");
+//            }
+//            return bandera;
+            
+            while(true){
+            if(reservao.contains(generarCadena()))
+            {
+                System.out.println("Argumento valido");
+             return true;   
+            }else
+            {
+                System.out.println("Argumento no valido");
+                return false;
+            }
+            }
+            
+    }
+
+    
 }
